dc57aedca312adea0ce1b1c3ba918e9bf55c522e
Thiago Caldas De Araujo <thiago.caldas@estacio.br>
2019-09-10 15:17:49 -0300

    Ajuste gitignore 2

--- Censo.API/ADODB/Connection.cs
+++ Censo.API/ADODB/Connection.cs
@@ -1,17 +1,20 @@
 using System.Data;
 using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace Censo.API.ADODB
 {
     public static class Connection
     {
 
+        public static IConfiguration Configuration { get; }
+
         private static SqlConnection conn;
 
         public static SqlConnection Get()
         {
-            
-            string strConn = "Data Source=db-alteryx.database.windows.net;Database=db-alteryx;User Id=db-admin;Password=8v&Kmu8b;";
+            string config = Configuration.GetConnectionString("DefaultConnection");
+            string strConn = config;
             conn = new SqlConnection(strConn);
             conn.Open();
             return conn;
