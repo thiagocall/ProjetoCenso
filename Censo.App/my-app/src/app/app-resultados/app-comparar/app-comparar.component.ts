d9bcdf3c9b3ee86954b4e471b55a04ce39b5f924
Thiago Caldas De Araujo <thiago.caldas@estacio.br>
2019-11-25 14:02:46 -0300

    Inclusão tela comparação Resultados app e api

---
+++ Censo.App/my-app/src/app/app-resultados/app-comparar/app-comparar.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { OtimizacaoService } from 'src/app/_services/otimizacao.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-app-comparar',
+  templateUrl: './app-comparar.component.html',
+  styleUrls: ['./app-comparar.component.css']
+})
+export class AppCompararComponent implements OnInit {
+
+  constructor(private OtimizacaoService: OtimizacaoService, private router: Router, private route: ActivatedRoute ) { }
+
+  comparar: any;
+  resultados: any;
+  resultadosJson: any;
+
+
+
+  ngOnInit() {
+
+    this.route.queryParams
+      .subscribe(params =>
+        this.comparar = params.res);
+
+    this.Comparar();
+
+  }
+
+
+  Comparar() {
+    console.log(this.comparar);
+    this.OtimizacaoService.getComparaResultado(this.comparar)
+      .subscribe(
+        response => {
+          this.resultados = response;
+          
+           console.log(this.resultados);
+        },
+        error => {
+
+        }
+      );
+
+  }
+
+  JSONParse_ (js: any) {
+     return JSON.parse(js);
+  }
+
+}
