8af124cfaea42599c330a0b790bbb686d0f64e43
joyce sena <joycesenanet@gmail.com>
2020-03-20 19:38:48 -0300

    ciclo enade

--- Censo.App/my-app/src/app/manutencao-curso-enade/manutencao-curso-enade.component.ts
+++ Censo.App/my-app/src/app/manutencao-curso-enade/manutencao-curso-enade.component.ts
@@ -35,13 +35,37 @@ export class ManutencaoCursoEnadeComponent implements OnInit {
     );
   }
 
+  segundaLista() {
+    this.enadeService.obterCiclos().subscribe(
+      response => {
+        this.todosCiclos = response;
+        this.id = this.todosCiclos.idCiclo;
+        //console.log(this.todosCiclos);
+        //console.log(this.todosCiclos[2]);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
 
   filtrarCiclo(id:number){
    this.cicloFiltrado = this.todosCiclos.filter(x => x.idCiclo == id)[0] // [0] para pegar o primeira posicao do array o filtre pega só um array por veze para acessar esse obj dentro do array
   // console.log(this.cicloFiltrado[0].descArea)
   }
 
- 
+ adicionarArea() {
+  console.log('Adicionar');
+ }
+
+ removerArea() {
+  console.log('Remover');
+ }
+
+ salvar(){
+   console.log('Salvar');
+ }
 
 
 }
